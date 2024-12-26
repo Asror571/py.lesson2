dd28dd930ace8924c857b65b43d856c7f90dfb77
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:51:11 +0500

    successful

---
+++ 1-masala.py
@@ -0,0 +1,9 @@
+print("Assalomu alaykum !")
+
+#Userdan ma'lumotlalr olindi 
+text = input("Matn kiriting :")
+
+#Natija chiqarildi 
+print("Natija :",text.upper())
+
+print("Bizni tanlaganingiz uchun Tashakkur!")
