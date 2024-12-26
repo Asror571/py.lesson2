1f68fc3671a116dfbe5d5a2cb007cb18cea71af3
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:52:22 +0500

    successful

---
+++ 5-masala.py
@@ -0,0 +1,7 @@
+print("Assalomu Alaykum !")
+text = input("Matn kiriting : ") #Userdan matn olindi 
+
+for i in range(5) :   # Loop orqali chiqarildi 
+    print(text)
+
+print("Bizni tanlaganingiz uchun Tashakkur!")
