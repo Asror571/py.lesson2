1a731ad482cf3ac986c23359f528745ab4d7fce3
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:51:49 +0500

    successful

---
+++ 3-masala.py
@@ -0,0 +1,11 @@
+print("Assalomu alaykum !")
+
+#Userdan matn va qidirilayotgan harf olindi 
+text = input("Matn kiriting !")
+letter = input("Qaysi harfni qidirayabsiz!")
+
+#Count funksiyasi yordamida nechta ishtirok etgani aniqlab olindi 
+count_letter = text.count(letter)
+
+#Natija chiqarildi 
+print(f"Siz qidirayotgan {letter} harfi  {count_letter}  ta ishtirok etgan " )
