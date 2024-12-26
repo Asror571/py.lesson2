0a6faefe6db8ee359e0ebb5c3db9055d3d314cab
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 09:07:43 +0500

    successful

---
+++ 14-masala.py
@@ -0,0 +1,8 @@
+print("Assalomu alaykum!")
+
+#USerdan son olindi 
+number = int(input("Sonni kiriting "))
+
+for  i in range(1,number+1):
+    if number % i == 0:      # if yordamida tekshirildi 
+        print(i)    # Natija chiqarildi 
