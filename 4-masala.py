89b03f34ec330559f9f762451476982f547b0c2e
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:52:05 +0500

    successful

---
+++ 4-masala.py
@@ -0,0 +1,7 @@
+print("Assalomu Alaykum!")
+age = int(input("Yoshini kiriitng :"))  #Userdan yoshi so'raldi va olindi 
+
+if age > 18:                      # Shart berildi agar 18 yoshdan oshgan bo'lsa 
+    print("Voyaga yetgan")
+else :                             #Aks xolda 
+    print("Voyaga yetmagan")   
