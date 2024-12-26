afb8f8d91b6c769441d062fc5177f9e55acdf67a
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:53:49 +0500

    successful

---
+++ 7-masala.py
@@ -0,0 +1,11 @@
+print("Assalomu Alaykum !")
+
+#Userdan matn va qidirilishi kerak bo'lgan so'z olindi 
+text = input("Matn kiriting !")
+word = input("Qidirayotgan so'zni kiriting!")
+
+#Agar mavjud bo'lsa shart ishlaydi 
+if word in word :
+    print(f'Siz qidirayotgan :"{ word }" matn ichida mavjud!')
+else :
+    print(f"Siz qidirayotgan{word} matn ichida mavjud emas x!")
