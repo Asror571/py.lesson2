0781700a1ddc7d05847029a53b149006ac78a7b2
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:52:40 +0500

    successful

---
+++ 6-masala.py
@@ -0,0 +1,11 @@
+print("Assalomu alaykum !")
+
+#Userdan matn olidi
+text = input("Matn kiriting :")
+
+#Barcha so'zlarni bosh harfi Katta harfga o'zgartirildi 
+word_upper = text.title()
+
+#Natija chiqarildi 
+print("Natija :",word_upper)
+print("Bizni tanlaganingiz uchun Tashakkur!")
