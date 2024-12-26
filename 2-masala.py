03f446911e15aa7ec8111827d316462aa908285c
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 08:51:30 +0500

    successful

---
+++ 2-masala.py
@@ -0,0 +1,14 @@
+print("Assalomu alaykum !")
+
+#Userdan ma'lumot olidi
+text = input("Matn kiriting :")
+
+#So'zlar olnidi 
+matn = text.split()
+
+#Hisoblash uchun o'zgaruvchi yaratildi 
+word_count = len(matn)
+
+#Natija chiqarildi 
+print("Natija :",word_count)
+print("Bizni tanlaganingiz uchun Tashakkur!")
