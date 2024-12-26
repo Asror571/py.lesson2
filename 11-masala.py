5335f383451f06c12255c5724d8cef044279fa3b
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-26 09:01:23 +0500

    successful

---
+++ 11-masala.py
@@ -0,0 +1,8 @@
+print("Asalomu Alaykum Bratim! Qanaysiz")
+
+#Usredan matn olindi 
+matn = input("Biror bir so'z kiriting :")
+
+# Palindrom yoki Palindrom emasligi tekshirildi 
+print(matn == matn[::-1])
+    
